9c34bee503f9a5410b1c7b61d28c272d07912d58
Ruben <rubenchotr1629@gmail.com>
2021-11-05 13:09:56 -0500

    first commit

---
+++ book-backend/index.js
@@ -0,0 +1,19 @@
+
+// impotart libraries
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParse = ('body-parser');
+const cors = require ('cors');
+
+// use methods libs.
+const app = express();
+require('dotenv').config();
+
+// Middlewares
+
+// Database setup
+
+// Routes Setup
+
+// Listen to Port
